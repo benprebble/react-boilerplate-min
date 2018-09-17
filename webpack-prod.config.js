8f15edcbcb340ab18c1e6ca3b79db9077a5b46fa
bprebble <ben.prebble@gmail.com>
2018-09-17 19:22:07 +1200

    initial commit

---
+++ webpack-prod.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+    mode: "production",
+    entry: ["babel-polyfill", "./src/index.js"],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/preset-env"], plugins: ["transform-class-properties"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|pdf)/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "file-loader",
+                options: {
+                    name: "/images/[name].[ext]"
+                }
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        filename: "bundle.js"
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(["dist"]),
+        new HtmlWebpackPlugin({
+            title: "React Starter",
+            template: path.resolve(__dirname, "public/index.html")
+        })
+    ]
+};
